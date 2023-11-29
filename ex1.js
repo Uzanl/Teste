63adf271f4e0590352a8dfd36ef8667c15754f1c
Uzann <Uzann@DESKTOP-EQ3MGO4>
2023-11-29 16:51:59 -0300

    código ex1 no modelo orientado ao objeto

--- ex1.js
+++ ex1.js
@@ -1,22 +1,26 @@
-// Função para calcular a porcentagem de votos
-function calcularPorcentagemVotos(votosOpcao, totalVotos) {
-    return (votosOpcao / totalVotos) * 100;
+class CalculadoraVotos {
+    constructor(totalVotos, votosValidos, votosBrancos, votosNulos) {
+        this.totalVotos = totalVotos;
+        this.votosValidos = votosValidos;
+        this.votosBrancos = votosBrancos;
+        this.votosNulos = votosNulos;
+    }
+
+    calcularPorcentagemVotos(votos) {
+        return (votos / this.totalVotos) * 100;
+    }
+
+    exibirPorcentagemVotos() {
+        const porcentagemValidos = this.calcularPorcentagemVotos(this.votosValidos);
+        const porcentagemBrancos = this.calcularPorcentagemVotos(this.votosBrancos);
+        const porcentagemNulos = this.calcularPorcentagemVotos(this.votosNulos);
+
+        console.log(`Porcentagem de votos para Válidos: ${porcentagemValidos.toFixed(2)}%`);
+        console.log(`Porcentagem de votos para Brancos: ${porcentagemBrancos.toFixed(2)}%`);
+        console.log(`Porcentagem de votos para Nulos: ${porcentagemNulos.toFixed(2)}%`);
+    }
 }
 
-// Número total de votos
-const totalVotos = 1000; // Substitua pelo número total de votos
-
-// Número de votos para cada opção
-const validos = 800; // Substitua pelo número de votos para a opção A
-const votosbrancos = 150; // Substitua pelo número de votos para a opção B
-const nulos = 50; // Substitua pelo número de votos para a opção C
-
-// Calculando a porcentagem de votos para cada opção
-const porcentagemOpcaoA = calcularPorcentagemVotos(validos, totalVotos);
-const porcentagemOpcaoB = calcularPorcentagemVotos(votosbrancos, totalVotos);
-const porcentagemOpcaoC = calcularPorcentagemVotos(nulos, totalVotos);
-
-// Exibindo os resultados
-console.log(`Porcentagem de votos para a Opção A: ${porcentagemOpcaoA.toFixed(2)}%`);
-console.log(`Porcentagem de votos para a Opção B: ${porcentagemOpcaoB.toFixed(2)}%`);
-console.log(`Porcentagem de votos para a Opção C: ${porcentagemOpcaoC.toFixed(2)}%`);
+// Utilização da classe CalculadoraVotos
+const calculadora = new CalculadoraVotos(1000, 800, 150, 50);
+calculadora.exibirPorcentagemVotos();
